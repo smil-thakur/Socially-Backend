3488750f8d999b2328c1d5109728f07203333003
smil-thakur <smil.raj.thakur@gmail.com>
2025-09-21 14:11:40 +0530

    update cors for deployment

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://smil-thakur.github.io/Socially"],
+    allow_origins=["https://smil-thakur.github.io",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
